2fe3c36993e1195af4c15357165f7330812659ec
rodgnathaniel <rodgnathaniel@gmail.com>
2019-04-15 11:59:12 -0700

    started node project

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+// Init app
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+//Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Index route 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//Define port
+const port = 3000;
+
+//Start Server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+
