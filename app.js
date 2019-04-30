d3db2aa04157c963c3346f89d606715eb6583c05
rodgnathaniel <rodgnathaniel@gmail.com>
2019-04-29 20:36:18 -0700

    \

--- app.js
+++ app.js
@@ -4,30 +4,37 @@ const ejs = require('ejs');
 const Nexmo = require('nexmo');
 const socketio = require('socket.io');
 
+
 //Init Nexmo 
 const nexmo = new Nexmo({
     apiKey: '67cd2a8d',
     apiSecret: 'rv07VthsSxDdvxTd'
 }, {debug: true});
 
+
 // Init app
 const app = express();
+
 
 // Template engine setup
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
 
+
 //Public folder setup
 app.use(express.static(__dirname + '/public'));
+
 
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 //Index route 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
 
 //Catch form submit 
 app.post('/', (req, res) => {
@@ -56,11 +63,14 @@ app.post('/', (req, res) => {
     )
 });
 
+
 //Define port
 const port = 3000;
 
+
 //Start Server
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+
 
 //Connect to socket.io
 const io = socketio(server);
